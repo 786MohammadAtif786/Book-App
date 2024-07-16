aabdaa4b86f333e94366556c3fd098a3496a54db
786MohammadAtif786 <mohd.atif81999@gmail.com>
2024-07-16 19:23:32 +0530

    nodemon add and borrow controlller error handling

--- controllers/borrowController.js
+++ controllers/borrowController.js
@@ -1,33 +1,39 @@
-const Book = require('../models/Book');
-const response = require('../utils/response');
+const Book = require("../models/Book");
+const response = require("../utils/response");
 
 const borrowBook = async (req, res) => {
- try {
-  const { bookId, userId } = req.body;
-  const book = await Book.findById(bookId);
-  if (!book || book.borrower) {
-    return response(res, false, null, 'Book not available for borrowing.', 400);
+  try {
+    const { bookId, userId } = req.body;
+    const book = await Book.findById(bookId);
+    if (!book || book.borrower) {
+      return response(
+        res,
+        false,
+        null,
+        "Book not available for borrowing.",
+        400
+      );
+    }
+    book.borrower = userId;
+    await book.save();
+    return response(res, true, book, "Book borrowing.", 200);
+  } catch (err) {
+    return response(res, false, null, "internal server error.", 500);
   }
-  book.borrower = userId;
-  await book.save();
-  return response(res, true, book, 'Book borrowing.', 200);
- } catch(err) {
-  return response(res, false, null, 'internal server error.', 500);
- }
 };
 
 const returnBook = async (req, res) => {
- try {
-  const book = await Book.findById(req.params.id);
-  if (!book || !book.borrower) {
-    return response(res, false, null, 'Book not borrowed.', 400);
+  try {
+    const book = await Book.findById(req.params.id);
+    if (!book || !book.borrower) {
+      return response(res, false, null, "Book not borrowed.", 400);
+    }
+    book.borrower = null;
+    await book.save();
+    return response(res, true, book, "return book.", 200);
+  } catch (err) {
+    return response(res, false, null, "internal server error.", 500);
   }
-  book.borrower = null;
-  await book.save();
-  return response(res, true, book, 'return book.', 200);
- } catch(err) {
-  return response(res, false, null, 'internal server error.', 500);
- }
 };
 
 module.exports = { borrowBook, returnBook };
